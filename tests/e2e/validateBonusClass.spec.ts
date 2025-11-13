83d0f8abcf15f56a21abf699bbf61740806d864a
youserCaio <caio.bernardinli@youse.com.br>
2025-11-13 14:39:22 -0300

    cycle test VND-1047

--- tests/e2e/validateBonusClass.spec.ts
+++ tests/e2e/validateBonusClass.spec.ts
@@ -5,7 +5,7 @@ test.describe('Validate Bonus Class', () => {
     test.describe('bonus class "Não" - hasBonus: false', () => {
         test.describe.configure({mode: 'serial'});
 
-        test('Not use bonus class', async ({bonusesClass, hasBonus}) => {
+        test('Not use bonus class', async ({bonusesClass, hasBonus, page}) => {
             expect(hasBonus).toBe(false);
         });
     });
@@ -16,16 +16,17 @@ test.describe('Validate Bonus Class', () => {
         test.describe('with UserBonusClass.ONE', () => {
             test.use({hasBonus: true, userBonusClass: UserBonusClass.ONE});
             
-            test('Use Bonus Class 1', async ({bonusesClass, hasBonus, userBonusClass}) => { 
+            test('Use Bonus Class 1', async ({bonusesClass, hasBonus, userBonusClass, page}) => { 
                 expect(hasBonus).toBe(true);
                 expect(userBonusClass).toBe(UserBonusClass.ONE);
+                
             });
         });
         
         test.describe('with UserBonusClass.NAN', () => {
             test.use({hasBonus: true, userBonusClass: UserBonusClass.NAN});
             
-            test('Use Bonus Class NaN', async ({bonusesClass, hasBonus, userBonusClass}) => { 
+            test('Use Bonus Class NaN', async ({bonusesClass, hasBonus, userBonusClass, page}) => { 
                 expect(hasBonus).toBe(true);
                 expect(userBonusClass).toBe(UserBonusClass.NAN);
             });
