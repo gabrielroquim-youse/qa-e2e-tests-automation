2024e1217c45c9a64e507e1aaaabf73f540a67f4
Marcelo Carmo <marcelo.carmo@youse.com.br>
2026-01-02 15:00:02 -0300

    Add optional paramenter to policy creation service

--- tests/services/TestUtilsService.ts
+++ tests/services/TestUtilsService.ts
@@ -3,7 +3,7 @@ import { TestConfig } from '../../config/test.config';
 import { Product } from '../enum/Product';
 import { log } from 'node:console';
 
-interface TestUtilsReponse {
+interface TestUtilsResponse {
   flow: string;
   protocol_number: string;
   status: string;
@@ -11,30 +11,47 @@ interface TestUtilsReponse {
   failure_reasons: string[];
 }
 
+interface TestUtilsPolicyResponse {
+  data: {
+    email: string;
+    password: string;
+    policy_number: string;
+  };
+}
+
 export class TestUtilsService {
   static async getByProtocolNumber(request: APIRequestContext, protocolNumber: string) {
     return await (await request.get(`${TestConfig.urls.qa.testUtilsUrl}/v1/orders/${protocolNumber}`)).json();
   }
 
-  static async createInsurancePolicy(request: APIRequestContext, product: Product): Promise<TestUtilsReponse> {
+  static async createInsurancePolicy(
+    request: APIRequestContext,
+    product: Product = Product.AUTO,
+    data?: { email?: string; license_plate?: string; documentNumber?: string; installmentsPerYear?: string; partnerId?: string },
+  ): Promise<TestUtilsPolicyResponse> {
     const response = await request.post(`${TestConfig.urls.qa.testUtilsUrl}/v1/orders`, {
       data: {
         flow: 'create_insurance_policy',
         data: {
           product: product,
+          user_email: data?.email || null,
+          vehicle_license_plate: data?.license_plate || null,
+          insured_person_document_number: data?.documentNumber || null,
+          payment_conditions_installments_per_year: data?.installmentsPerYear || null,
+          partner_id: data?.partnerId || null,
         },
       },
     });
 
     const protocolNumber = (await response.json()).protocol_number;
 
-    let orderData: TestUtilsReponse;
+    let orderData: TestUtilsResponse;
 
-    for (let attempt = 0; attempt < 7; attempt++) {
+    for (let attempt = 0; attempt < 8; attempt++) {
       orderData = await this.getByProtocolNumber(request, protocolNumber);
       log(`Attempt ${attempt + 1}: Order status is ${orderData.status}`);
       if (orderData.status === 'done') {
-        return orderData!;
+        return orderData.data as TestUtilsPolicyResponse;
       } else if (orderData.status === 'failed') {
         throw new Error(`Order creation failed. Reason: ${orderData.failure_reasons}`);
       }
